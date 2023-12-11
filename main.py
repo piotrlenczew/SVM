Incorrect results, shoukd probably rollback 2 commits

main.py
@@ -8,7 +8,7 @@ wine_quality = fetch_ucirepo(id=186)
 X = wine_quality.data.features
 y = wine_quality.data.targets
 
-y_binary = np.where(y > 5, 1, 0)
+y_binary = np.where(y > 5, 1, -1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.2, random_state=42)
 
@@ -19,7 +19,7 @@ X_test_scaled = scaler.transform(X_test)
 print(X_train_scaled)
 print(X_test_scaled)
 
-svm_params = SVMParams(C=1.0, kernel='rbf', sigma=0.5)
+svm_params = SVMParams()
 svm = SVM(svm_params)
 svm.fit(X_train_scaled, y_train)
 print("Train score", svm.score(X_test_scaled, y_test))

svm.py
@@ -43,7 +43,7 @@ class SVM:
 
     def _calculate_bias(self):
         index = np.where((self.alpha) > 0 & (self.alpha < self.params.C))[0]
-        b_i = self.y[index] - (self.alpha * self.y).dot(self.kernel(self.X, self.X[index]))
+        b_i = self.y[index] - (self.alpha[index] * self.y[index]).dot(self.kernel(self.X[index], self.X[index]))
         return np.mean(b_i)
 
     def _gradient(self, yi, xi):
@@ -70,9 +70,7 @@ class SVM:
             loss = np.sum(self.alpha) - 0.5 * np.sum(np.outer(self.alpha, self.alpha) * y_iy_jk_ij)
             losses.append(loss)
 
-        index = np.where((self.alpha) > 0 & (self.alpha < self.params.C))[0]
-        b_i = self.y[index] - np.sum(self.alpha * self.y * self.kernel(self.X, self.X[index]), axis=0)
-        self.bias = np.mean(b_i)
+        self.bias = self._calculate_bias()
 
         plt.plot(losses)
         plt.title("loss per epochs")