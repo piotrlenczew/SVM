e73458da9a085217d7c86cf5bf496c05f55ec303
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-12-11 20:54:17 +0100

    Realy slow implementation of SVM

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-
-
+from svm import SVM, SVMParams
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 from ucimlrepo import fetch_ucirepo
 wine_quality = fetch_ucirepo(id=186)
 
 X = wine_quality.data.features
-Y = wine_quality.data.targets
+y = wine_quality.data.targets
 
-print(wine_quality.metadata)
-print(wine_quality.variables)
+y_binary = np.where(y > 5, 1, 0)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+print(X_train_scaled)
+print(X_test_scaled)
+
+svm_params = SVMParams(C=1.0, kernel='rbf', sigma=0.5)
+svm = SVM(svm_params)
+svm.fit(X_train_scaled, y_train)
+print("Train score", svm.score(X_test_scaled, y_test))
--- svm.py
+++ svm.py
@@ -1,20 +1,16 @@
 import numpy as np
-from autograd import grad
+import matplotlib.pyplot as plt
 
 
 class SVMParams:
     def __init__(
         self,
-        lambda_param: float = 0.01,
-        num_iter: int = 1000,
-        learning_rate: float = 0.01,
+        C: float = 1.0,
         kernel: str = 'linear',
         degree: int = 3,
         sigma: float = 0.5,
     ):
-        self.lambda_param = lambda_param
-        self.num_iter = num_iter
-        self.learning_rate = learning_rate
+        self.C = C
         self.kernel = kernel
         self.degree = degree
         self.sigma = sigma
@@ -23,9 +19,9 @@ class SVMParams:
 class SVM:
     def __init__(self, params: SVMParams):
         self.params = params
-        self.X_train = None
-        self.Y_train = None
-        self.alphas = None
+        self.X = None
+        self.y = None
+        self.alpha = None
         self.bias = None
 
     def _linear_kernel(self, u, v):
@@ -37,7 +33,7 @@ class SVM:
     def _rbf_kernel(self, u, v):
         return np.exp(-np.linalg.norm(u - v) ** 2 / (2 * self.params.sigma ** 2))
 
-    def _apply_kernel(self, u, v):
+    def _kernel(self, u, v):
         if self.params.kernel == 'linear':
             return self._linear_kernel(u, v)
         elif self.params.kernel == 'polynomial':
@@ -47,40 +43,50 @@ class SVM:
         else:
             raise ValueError("Unsupported kernel type")
 
-    def _hinge_loss(self, alphas, bias):
-        loss = []
-        for i in range(self.X_train.shape[0]):
-            f = np.sum(alphas[i] * self.Y_train[i] * self._apply_kernel(self.X_train[i], self.X_train) - bias)
-            loss.append(max(1-np.dot(f, self.Y_train[i]), 0))
-        return max(loss)
+    def _loss(self):
+        return np.sum(self.alpha) - 0.5 * np.sum(np.outer(self.alpha, self.alpha) * np.outer(self.y, self.y) * self._kernel(self.X, self.X))
 
-    def _regularization_term(self, alphas):
-        sum = 0.0
-        for i in range(self.X_train.shape[0]):
-            for j in range(self.X_train.shape[0]):
-                sum += np.dot(alphas[i]*alphas[j]*self.Y_train[i]*self.Y_train[j], self._apply_kernel(self.X_train[i], self.X_train[j]))
-        return self.params.lambda_param * sum
+    def _calculate_bias(self):
+        index = np.where((self.alpha) > 0 & (self.alpha < self.params.C))[0]
+        b_i = self.y[index] - (self.alpha * self.y).dot(self._kernel(self.X, self.X[index]))
+        return np.mean(b_i)
 
-    def _objective_function(self, alphas, bias):
-        return self._hinge_loss(alphas, bias) + self._regularization_term(alphas)
+    def _learning_rate(self, xi):
+        return 1/self._kernel(xi, xi)
 
-    def fit(self, X, Y):
-        self.X_train = X
-        self.Y_train = Y
-        num_samples, num_features = X.shape
-        self.alphas = np.zeros(num_samples)
+    def _gradient(self, yi, xi):
+        return 1 - yi * np.sum([self.alpha[j] * self.y[j] * self._kernel(self.X[j], xi) for j in range(self.X.shape[0])])
+
+    def fit(self, X, y, epochs=500):
+        self.X = X
+        self.y = y
+
+        self.alpha = np.random.random(X.shape[0])
         self.bias = 0
 
-        objective_gradient = grad(self._objective_function, argnums=(0, 1))
+        losses = []
+        for _ in range(epochs):
+            for i in range(X.shape[0]):
+                gradient = self._gradient(y[i], X[i])
+                self.alpha[i] = self.alpha[i] + self._learning_rate(X[i]) * gradient
 
-        for _ in range(self.params.num_iter):
-            gradient_alphas, gradient_bias = objective_gradient(self.alphas, self.bias)
+            self.alpha[self.alpha > self.params.C] = self.params.C
+            self.alpha[self.alpha < 0] = 0
 
-            self.alphas -= self.params.learning_rate * gradient_alphas
-            self.bias -= self.params.learning_rate * gradient_bias
+            losses.append(self._loss())
 
-    def _decision_function(self):
-        return
+        self.bias = self._calculate_bias()
+
+        plt.plot(losses)
+        plt.title("loss per epochs")
+        plt.show()
+
+    def _decision_function(self, X):
+        return (self.alpha * self.y).dot(self._kernel(self.X, X)) + self.bias
 
     def predict(self, X):
-        for i in range()
+        return np.sign(self._decision_function(X))
+
+    def score(self, X, y):
+        y_hat = self.predict(X)
+        return np.mean(y == y_hat)
