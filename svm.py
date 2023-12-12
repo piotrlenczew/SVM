b8e9425ab6a8e255faae29afa81724c249e81338
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-12-12 11:32:41 +0100

    Way too slow implementation

--- svm.py
+++ svm.py
@@ -1,16 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from autograd import grad
 
 
 class SVMParams:
     def __init__(
         self,
-        C: float = 1.0,
+        lambda_param: float = 0.01,
         kernel: str = 'linear',
         degree: int = 3,
         sigma: float = 0.5,
     ):
-        self.C = C
+        self.lambda_param = lambda_param
         self.kernel = kernel
         self.degree = degree
         self.sigma = sigma
@@ -27,54 +27,43 @@ class SVM:
             self.kernel = self._rbf_kernel
         else:
             raise ValueError("Unsupported kernel type")
+
         self.X = None
         self.y = None
         self.alpha = None
         self.bias = None
 
     def _linear_kernel(self, u, v):
-        return u.dot(v.T)
+        return np.dot(u.T, v)
 
     def _polynomial_kernel(self, u, v):
-        return (1 + u.dot(v.T)) ** self.params.degree
+        return (1 + np.dot(u.T, v)) ** self.params.degree
 
     def _rbf_kernel(self, u, v):
-        return np.exp(-(1 / self.params.sigma ** 2) * np.linalg.norm(u[:, np.newaxis] - v[np.newaxis, :], axis=2) ** 2)
+        return np.exp(-np.linalg.norm(u - v) ** 2 / (2 * self.params.sigma ** 2))
 
-    def _calculate_bias(self):
-        index = np.where((self.alpha) > 0 & (self.alpha < self.params.C))[0]
-        b_i = self.y[index] - np.sum((self.alpha * self.y).reshape(-1, 1)*self.kernel(self.X, self.X[index]), axis=0)
-        return np.mean(b_i)
+    def _hinge_loss(self, alpha, bias):
+        return np.maximum(1 - np.dot(alpha * self.y, self.kernel(self.X, self.X)) - self.bias)
 
-    def _gradient(self, yi, xi):
-        return 1 - yi * np.sum([self.alpha[j] * self.y[j] * self.kernel(self.X[j], xi) for j in range(self.X.shape[0])])
+    def _regularization_term(self, alpha):
+        return self.params.lambda_param * np.dot(alpha * alpha * self.y * self.y, self.kernel(self.X, self.X))
+
+    def _objective_function(self, alpha, bias):
+        return self._hinge_loss(alpha, bias) + self._regularization_term(alpha)
 
     def fit(self, X, y, lr=1e-3, epochs=500):
         self.X = np.array(X)
         self.y = np.array(y)
-
-        self.alpha = np.random.random(X.shape[0])
+        self.alpha = np.random.random(X.shape)
         self.bias = 0
-        ones = np.ones(X.shape[0])
 
-        y_iy_jk_ij = np.outer(self.y, self.y) * self.kernel(self.X, self.X)
+        objective_gradient = grad(self._objective_function)
 
-        losses = []
         for _ in range(epochs):
-            gradient = ones - y_iy_jk_ij.dot(self.alpha)
-            self.alpha = self.alpha + lr * gradient
+            gradient_alphas, gradient_bias = objective_gradient(self.alpha, self.bias)
 
-            self.alpha[self.alpha > self.params.C] = self.params.C
-            self.alpha[self.alpha < 0] = 0
-
-            loss = np.sum(self.alpha) - 0.5 * np.sum(np.outer(self.alpha, self.alpha) * y_iy_jk_ij)
-            losses.append(loss)
-
-        self.bias = self._calculate_bias()
-
-        plt.plot(losses)
-        plt.title("loss per epochs")
-        plt.show()
+            self.alpha -= lr * gradient_alphas
+            self.bias -= lr * gradient_bias
 
     def _decision_function(self, X):
         return (self.alpha * self.y).dot(self.kernel(self.X, X)) + self.bias
